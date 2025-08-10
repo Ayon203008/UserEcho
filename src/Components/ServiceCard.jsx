e2e99717089c590dab23da0cb54405c9bc5a8c61
Ayon203008 <paypal572874@gmail.com>
2025-08-10 12:19:27 +0600

    button colour changed

--- src/Components/ServiceCard.jsx
+++ src/Components/ServiceCard.jsx
@@ -38,7 +38,7 @@ const ServiceCard = ({ service }) => {
         {/* Button */}
         <NavLink
           to={`/services/${_id}`}
-          className="inline-block w-full text-center bg-green-600 hover:bg-green-700 text-white font-medium py-2 rounded-lg transition-colors duration-300"
+          className="inline-block w-full text-center bg-blue-600 hover:bg-blue-900 text-white font-medium py-2 rounded-lg transition-colors duration-300"
         >
           See More
         </NavLink>
